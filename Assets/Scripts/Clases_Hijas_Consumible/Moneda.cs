5fa0748614e2f7854859120d5c2b2c4681919cd3
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2024-05-16 18:20:28 -0300

    esqueleto_consumibles_parte1

---
+++ Assets/Scripts/Clases_Hijas_Consumible/Moneda.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : Consumible
+{
+    public override void Destruir()
+    {
+        base.Destruir();
+    }
+}
---
+++ Assets/Scripts/Clases_Hijas_Consumible/Pocion_Velocidad.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocion_Velocidad : Consumible
+{
+    public override void Destruir()
+    {
+        base.Destruir();
+    }
+}

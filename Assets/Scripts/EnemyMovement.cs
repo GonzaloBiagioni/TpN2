ee69dbf478914d5264adeeb0740d727e6a6fde4f
GonzaloBiagioni <gonzalobiagioni3@gmail.com>
2024-05-17 21:30:40 -0300

    scripts

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -26,4 +26,75 @@ public class EnemyMovement : MonoBehaviour
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
     }
+    public float velocidad = 2f;
+    public int daño = 1;
+    public GameObject prefabMoneda;
+    public float tiempoDestruccion = 0f;
+
+    void start()
+    {
+        // Enemigo sigue al jugador
+        GameObject jugador = GameObject.FindGameObjectWithTag("Player");
+
+        if (jugador != null)
+        {
+            Vector3 direccion = jugador.transform.position - transform.position;
+            direccion.z = 0f;
+            transform.Translate(direccion.normalized * velocidad * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otro)
+    {
+        if (otro.CompareTag("Player"))
+        {
+            // Enemigo impacta al jugador
+            JugadorImpactado(otro.GetComponent<Player>());
+        }
+        else if (otro.CompareTag("Bala Player"))
+        {
+            // Enemigo impactado por una bala
+            BalaImpactada(otro.gameObject);
+        }
+    }
+
+    void JugadorImpactado(Player jugador)
+    {
+        // Restar vida al jugador
+        CanvasHP barraHPJugador = jugador.GetComponentInChildren<CanvasHP>();
+
+        if (barraHPJugador != null)
+        {
+          //  barraHPJugador.cambiarVidaActual(barraHPJugador.slider.value - daño);
+        }
+        // Destruir el enemigo
+        DestruirEnemigo();
+    }
+    void BalaImpactada(GameObject bala)
+    {
+        // Destruir la bala
+        Destroy(bala);
+
+        // Soltar monedas
+        SoltarMonedas();
+
+        // Destruir el enemigo
+        DestruirEnemigo();
+    }
+
+    void SoltarMonedas()
+    {
+        // Instanciar monedas
+        if (prefabMoneda != null)
+        {
+            Instantiate(prefabMoneda, transform.position, Quaternion.identity);
+        }
+    }
+
+    void DestruirEnemigo()
+    {
+        // Destruir el enemigo después de un tiempo
+        Destroy(gameObject, tiempoDestruccion);
+    }
 }
+

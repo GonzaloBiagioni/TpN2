b1214f71eee62fcc477efe9e2481762db60da127
IgnacioMartinezInfante <145930821+IgnacioMartinezInfante@users.noreply.github.com>
2024-06-16 18:14:33 -0300

    Evento_Dialogo_NPC

---
+++ Assets/Scripts/Scripts/Dialogo_NPC.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class Dialogo_NPC : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+
+    private void OnEnable()
+    {
+        NPC.onNPCInteract += ActivarDialogo;
+        NPC.onNPCExit += DesactivarDialogo;
+    }
+
+    private void OnDisable()
+    {
+        NPC.onNPCInteract -= ActivarDialogo;
+        NPC.onNPCExit -= DesactivarDialogo;
+    }
+
+    private void ActivarDialogo()
+    {
+        dialogueText.gameObject.SetActive(true);
+        dialogueText.text = "¡Hola, aventurero! ¿Cómo puedo ayudarte hoy?";
+    }
+
+    private void DesactivarDialogo()
+    {
+        dialogueText.gameObject.SetActive(false);
+    }
+}

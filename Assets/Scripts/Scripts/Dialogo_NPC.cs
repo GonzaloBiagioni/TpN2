95ffc146d38e7de384a0184dd218e3319e321ff8
videojuegoslagos <160129157+videojuegoslagos@users.noreply.github.com>
2024-06-18 19:50:50 -0300

    a

--- Assets/Scripts/Scripts/Dialogo_NPC.cs
+++ Assets/Scripts/Scripts/Dialogo_NPC.cs
@@ -20,7 +20,7 @@ public class Dialogo_NPC : MonoBehaviour
     private void ActivarDialogo()
     {
         dialogueText.gameObject.SetActive(true);
-        dialogueText.text = "¡Hola, aventurero! ¿Cómo puedo ayudarte hoy?";
+        dialogueText.text = "Te vendo un pollito";
     }
 
     private void DesactivarDialogo()
